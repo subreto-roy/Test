d102b7fcd98579a63b45816f1c7ddd85c6f760e7
subreto roy <sroy172182@bscse.uiu.ac.bd>
2023-03-26 21:50:47 +0600

    first commit

---
+++ product/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from config.g_model import TimeStampMixin
+# pylint: disable=no-member
+
+
+# Create your models here.
+class Variant(TimeStampMixin):
+    title = models.CharField(max_length=40, unique=True)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+
+class Product(TimeStampMixin):
+    title = models.CharField(max_length=255)
+    sku = models.SlugField(max_length=255, unique=True)
+    description = models.TextField()
+     #my
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+   
+
+
+class ProductImage(TimeStampMixin):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    file_path = models.URLField()
+
+
+class ProductVariant(TimeStampMixin,models.Model):
+    variant_title = models.CharField(max_length=255)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class ProductVariantPrice(TimeStampMixin,models.Model):
+    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+                                            related_name='product_variant_one')
+    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+                                            related_name='product_variant_two')
+    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+                                              related_name='product_variant_three')
+    price = models.FloatField()
+    stock = models.FloatField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+
---
+++ product/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from product.views.product import CreateProductView
+from product.views.variant import VariantView, VariantCreateView, VariantEditView
+from . import views
+from product.views import ProductListView
+
+app_name = "product"
+
+urlpatterns = [
+    
+    path('variants/', VariantView.as_view(), name='variants'),
+    path('variant/create', VariantCreateView.as_view(), name='create.variant'),
+    path('variant/<int:id>/edit', VariantEditView.as_view(), name='update.variant'),
+
+    # Products URLs
+    path('create/', CreateProductView.as_view(), name='create.product'),
+    path('list/', TemplateView.as_view(template_name='products/list.html', extra_context={
+        'product': True
+    }), name='list.product'),
+    path('summary/', views.summary, name='summary'),
+
+    path('product/', ProductListView.as_view(), name='product_list'),
+]
